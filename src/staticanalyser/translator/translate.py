1f59cad5659ce7fa6e4421fd037c38278fd87418
Elliot Hargreaves <elliot@elliothargreaves.com>
2019-02-24 11:42:23 +0000

    Add parser lookup by filetype

--- src/staticanalyser/translator/translate.py
+++ src/staticanalyser/translator/translate.py
@@ -7,17 +7,25 @@ import re
 
 
 def lookup_parser(extension: str) -> str:
-    pass
+    return config.get_languages_by_extension(extension)
+
+
+def get_file_extension(entity: TextIOWrapper) -> str:
+    return re.split(r'\.', entity.name)[-1] # TODO compile regex pattern for better performance
 
 
 def translate(translate_args: Namespace) -> int:
     # TODO lookup filetypes
-    filetype_parser_map: dict = {}
+    extension_set: set = set()
+    selected_parsers: set = set()
+
     entity: TextIOWrapper
     for entity in translate_args.input_files:
-        extension: str = re.split('\.', entity.name)
+        extension: str = get_file_extension(entity)
+        extension_set.add(extension)
+        for parser in lookup_parser(extension):
+            selected_parsers.add(parser)
 
-        print(path.abspath(entity.name))
 
     # TODO check current model
     return 0
